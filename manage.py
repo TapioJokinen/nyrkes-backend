fd0cca543bc82513a5ecc3961bbab0ccd7cbdcd7
Tapio Jokinen <tapio.jokinen@hotmail.com>
2022-11-05 18:23:20 +0200

    Add possibility to debug containerized application

--- manage.py
+++ manage.py
@@ -3,10 +3,19 @@
 import os
 import sys
 
+from django.conf import settings
+
 
 def main():
     """Run administrative tasks."""
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.django.local")
+
+    if settings.DEBUG:
+        if os.environ.get("RUN_MAIN") or os.environ.get("WERKZEUG_RUN_MAIN"):
+            import debugpy
+
+            debugpy.listen(("0.0.0.0", 8001))
+            print("Attached!")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
