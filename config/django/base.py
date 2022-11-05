b58bde3a92b4b84ce2c516fe799f2a8161e41b2a
Tapio Jokinen <tapio.jokinen@hotmail.com>
2022-11-05 18:22:39 +0200

    Add settings for restframework and simplejwt

--- config/django/base.py
+++ config/django/base.py
@@ -1,4 +1,5 @@
 import os
+from datetime import timedelta
 
 from config.env import BASE_DIR, env
 
@@ -8,7 +9,10 @@ LOCAL_APPS = [
     "menel",
 ]
 
-THIRD_PARTY_APPS = []
+THIRD_PARTY_APPS = [
+    "rest_framework",
+    "rest_framework_simplejwt.token_blacklist",
+]
 
 INSTALLED_APPS = [
     "django.contrib.admin",
@@ -90,3 +94,45 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "menel.User"
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": ("menel.authentication.CookieAuthentication",),
+    "DEFAULT_VERSIONING_CLASS": "rest_framework.versioning.AcceptHeaderVersioning",
+    "DEFAULT_VERSION": None,
+    "ALLOWED_VERSIONS": ["1.0"],
+    "VERSION_PARAM": "version",
+}
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ROTATE_REFRESH_TOKENS": False,
+    "BLACKLIST_AFTER_ROTATION": False,
+    "UPDATE_LAST_LOGIN": False,
+    "ALGORITHM": "HS256",
+    "VERIFYING_KEY": None,
+    "AUDIENCE": None,
+    "ISSUER": None,
+    "JWK_URL": None,
+    "LEEWAY": 0,
+    "AUTH_HEADER_TYPES": ("Bearer",),
+    "AUTH_HEADER_NAME": "HTTP_AUTHORIZATION",
+    "USER_ID_FIELD": "id",
+    "USER_ID_CLAIM": "user_id",
+    "USER_AUTHENTICATION_RULE": "rest_framework_simplejwt.authentication.default_user_authentication_rule",
+    "AUTH_TOKEN_CLASSES": ("rest_framework_simplejwt.tokens.AccessToken",),
+    "TOKEN_TYPE_CLAIM": "token_type",
+    "TOKEN_USER_CLASS": "rest_framework_simplejwt.models.TokenUser",
+    "JTI_CLAIM": "jti",
+    "SLIDING_TOKEN_REFRESH_EXP_CLAIM": "refresh_exp",
+    "SLIDING_TOKEN_LIFETIME": timedelta(minutes=5),
+    "SLIDING_TOKEN_REFRESH_LIFETIME": timedelta(days=1),
+    "AUTH_COOKIE_KEY_REFRESH": "refresh_token",
+    "AUTH_COOKIE_KEY_ACCESS": "access_token",
+    "AUTH_COOKIE_MAX_AGE": 3600 * 24 * 7,  # timdelta object, seconds or None.
+    "AUTH_COOKIE_DOMAIN": None,  # Cross-domain cookie.
+    "AUTH_COOKIE_PATH": "/",  # The path of the auth cookie.
+    "AUTH_COOKIE_SECURE": False,  # Whether the auth cookies should be secure (https:// only).
+    "AUTH_COOKIE_HTTP_ONLY": True,  # True in order to prevent client-side JavaScript from having access to the cookie.
+    "AUTH_COOKIE_SAME_SITE": "Strict",  # Whether to set the flag restricting cookie leaks on cross-site requests. This can be 'Lax', 'Strict', or None to disable the flag.
+}
