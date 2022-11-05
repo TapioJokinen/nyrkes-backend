a179938f97507a4195b3c045bb647c14c5bd2efe
Tapio Jokinen <tapio.jokinen@hotmail.com>
2022-11-05 12:51:40 +0200

    Create constants.py

---
+++ menel/utils/constants.py
@@ -0,0 +1,6 @@
+class Errors:
+    REQUIRED_FIELD_EMAIL = "Email is required."
+    REQUIRED_FIELD_FIRST_NAME = "First name is required."
+    REQUIRED_FIELD_LAST_NAME = "Last name is required."
+    SUPERUSER_NOT_STAFF = "Superuser must have is_staff=True."
+    SUPERUSER_NOT_SUPERUSER = "Superuser must have is_superuser=True."
