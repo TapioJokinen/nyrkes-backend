905c7ab95c89531f2e9011ab5fbcc0c35ef39d97
Tapio Jokinen <tapio.jokinen@hotmail.com>
2022-11-06 19:29:20 +0200

    Add new urls for token blacklisting and verifying

--- config/urls.py
+++ config/urls.py
@@ -2,11 +2,18 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 
-from menel.api.views.token import CookieTokenObtainPairView, CookieTokenRefreshView
+from menel.api.views.token import (
+    CookieTokenBlacklistView,
+    CookieTokenObtainPairView,
+    CookieTokenRefreshView,
+    CookieTokenVerifyView,
+)
 
 urlpatterns = [
     path("auth/token/", CookieTokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("auth/token/refresh/", CookieTokenRefreshView.as_view(), name="token_refresh"),
+    path("auth/token/blacklist/", CookieTokenBlacklistView.as_view(), name="token_blacklist"),
+    path("auth/token/verify/", CookieTokenVerifyView.as_view(), name="token_verify"),
     path("api/", include("menel.api.urls")),
 ]
 
