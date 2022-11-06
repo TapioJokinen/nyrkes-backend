cd33eaeefbe74167a51e63f1a73ff6d67b26e411
Tapio Jokinen <tapio.jokinen@hotmail.com>
2022-11-06 19:29:39 +0200

    Add cookie serializers for token blacklisting and verifying

--- menel/api/serializers/token.py
+++ menel/api/serializers/token.py
@@ -1,5 +1,9 @@
 from rest_framework_simplejwt.exceptions import InvalidToken
-from rest_framework_simplejwt.serializers import TokenRefreshSerializer
+from rest_framework_simplejwt.serializers import (
+    TokenBlacklistSerializer,
+    TokenRefreshSerializer,
+    TokenVerifySerializer,
+)
 
 
 class CookieTokenRefreshSerializer(TokenRefreshSerializer):
@@ -9,5 +13,24 @@ class CookieTokenRefreshSerializer(TokenRefreshSerializer):
         attrs["refresh"] = self.context["request"].COOKIES.get("refresh_token")
         if attrs["refresh"]:
             return super().validate(attrs)
-        else:
-            raise InvalidToken("No valid token found in cookie 'refresh_token'")
+        raise InvalidToken("No valid token found in cookie 'refresh_token'")
+
+
+class CookieTokenVerifySerializer(TokenVerifySerializer):
+    token = None
+
+    def validate(self, attrs):
+        attrs["token"] = self.context["request"].COOKIES.get("refresh_token")
+        if attrs["token"]:
+            return super().validate(attrs)
+        raise InvalidToken("No valid token found in cookie 'refresh_token'")
+
+
+class CookieTokenBlacklistSerializer(TokenBlacklistSerializer):
+    refresh = None
+
+    def validate(self, attrs):
+        attrs["refresh"] = self.context["request"].COOKIES.get("refresh_token")
+        if attrs["refresh"]:
+            return super().validate(attrs)
+        raise InvalidToken("No valid token found in cookie 'refresh_token'")
