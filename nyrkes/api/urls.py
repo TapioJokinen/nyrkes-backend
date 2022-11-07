ea38cabde47b8a516204384b19434d74ccefaa97
Tapio Jokinen <tapio.jokinen@hotmail.com>
2022-11-07 20:02:31 +0200

    Remove test API

--- nyrkes/api/urls.py
+++ nyrkes/api/urls.py
@@ -1,7 +1,3 @@
-from django.urls import path
+# from django.urls import path
 
-from nyrkes.api.views.test import TestView
-
-urlpatterns = [
-    path("test/", TestView.as_view(), name="test"),
-]
+urlpatterns = []
--- nyrkes/api/views/test.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.response import Response
-
-from nyrkes.api.base import BaseAPIView
-
-
-class TestView(BaseAPIView):
-    def get(self, request):
-        return Response(200)
