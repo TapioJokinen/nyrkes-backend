94824b866c0be8f74ea8de8513b1a0d27d690bf2
Tapio Jokinen <tapio.jokinen@hotmail.com>
2022-11-08 22:34:58 +0200

    Add django-cors-headers

--- config/django/base.py
+++ config/django/base.py
@@ -10,6 +10,7 @@ LOCAL_APPS = [
 ]
 
 THIRD_PARTY_APPS = [
+    "corsheaders",
     "rest_framework",
     "rest_framework_simplejwt.token_blacklist",
 ]
@@ -28,6 +29,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
--- config/django/local.py
+++ config/django/local.py
@@ -7,8 +7,9 @@ SECRET_KEY = env("SECRET_KEY", default="SET_SECRET_KEY")
 
 ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
 
-CORS_ALLOWED_ORIGINS = ["http://localhost:3000"]
-
 REST_FRAMEWORK["AUTH_COOKIE_SAME_SITE"] = None
 
 SIMPLE_JWT["SIGNING_KEY"] = SECRET_KEY
+
+CORS_ALLOWED_ORIGINS = ["http://localhost:3000"]
+CSRF_TRUSTED_ORIGINS = ["http://localhost:3000"]
