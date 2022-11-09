e081c6c43548d3633a68ad56c4563e5a89dcb7c3
Tapio Jokinen <tapio.jokinen@hotmail.com>
2022-11-09 17:46:14 +0200

    Add CORS_ALLOW_CREDENTIALS

--- config/django/local.py
+++ config/django/local.py
@@ -5,11 +5,14 @@ DEBUG = True
 
 SECRET_KEY = env("SECRET_KEY", default="SET_SECRET_KEY")
 
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
-
 REST_FRAMEWORK["AUTH_COOKIE_SAME_SITE"] = None
 
 SIMPLE_JWT["SIGNING_KEY"] = SECRET_KEY
 
+ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
+
 CORS_ALLOWED_ORIGINS = ["http://localhost:3000"]
+
 CSRF_TRUSTED_ORIGINS = ["http://localhost:3000"]
+
+CORS_ALLOW_CREDENTIALS = True
