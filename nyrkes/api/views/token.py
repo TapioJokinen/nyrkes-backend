b77514aa35ff01fe961fef1c363d0b9edfd2289b
Tapio Jokinen <tapio.jokinen@hotmail.com>
2022-11-09 19:35:54 +0200

    Change blacklist tokens api to return always http ok

--- nyrkes/api/views/token.py
+++ nyrkes/api/views/token.py
@@ -1,4 +1,6 @@
 from django.conf import settings
+from rest_framework import status
+from rest_framework.response import Response
 from rest_framework_simplejwt.views import (
     TokenBlacklistView,
     TokenObtainPairView,
@@ -77,4 +79,38 @@ class CookieTokenVerifyView(TokenVerifyView):
 
 
 class CookieTokenBlacklistView(TokenBlacklistView):
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+
+        try:
+            serializer.is_valid(raise_exception=True)
+        except Exception:  # pylint: disable=broad-except
+            # Exception might be raised is user manually deleted refresh_token cookie.
+            # We still want to return response so cookies get cleaned.
+            pass
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    def finalize_response(self, request, response, *args, **kwargs):
+        response.set_cookie(
+            key=settings.SIMPLE_JWT["AUTH_COOKIE_KEY_REFRESH"],
+            value="",
+            max_age=settings.SIMPLE_JWT["AUTH_COOKIE_REFRESH_MAX_AGE"],
+            httponly=settings.SIMPLE_JWT["AUTH_COOKIE_HTTP_ONLY"],
+            domain=settings.SIMPLE_JWT["AUTH_COOKIE_DOMAIN"],
+            samesite=settings.SIMPLE_JWT["AUTH_COOKIE_SAME_SITE"],
+            secure=settings.SIMPLE_JWT["AUTH_COOKIE_SECURE"],
+        )
+
+        response.set_cookie(
+            key=settings.SIMPLE_JWT["AUTH_COOKIE_KEY_ACCESS"],
+            value="",
+            max_age=settings.SIMPLE_JWT["AUTH_COOKIE_ACCESS_MAX_AGE"],
+            httponly=settings.SIMPLE_JWT["AUTH_COOKIE_HTTP_ONLY"],
+            domain=settings.SIMPLE_JWT["AUTH_COOKIE_DOMAIN"],
+            samesite=settings.SIMPLE_JWT["AUTH_COOKIE_SAME_SITE"],
+            secure=settings.SIMPLE_JWT["AUTH_COOKIE_SECURE"],
+        )
+        return super().finalize_response(request, response, *args, **kwargs)
+
     serializer_class = CookieTokenBlacklistSerializer
