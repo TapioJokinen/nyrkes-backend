355c785295a4f151c329b35ab9b7cd621a06e809
Tapio Jokinen <tapio.jokinen@hotmail.com>
2022-11-13 21:37:58 +0200

    Move admin panel related stuff to local.py

--- config/django/base.py
+++ config/django/base.py
@@ -16,11 +16,9 @@ THIRD_PARTY_APPS = [
 ]
 
 INSTALLED_APPS = [
-    "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
-    "django.contrib.messages",
     "django.contrib.staticfiles",
     *LOCAL_APPS,
     *THIRD_PARTY_APPS,
@@ -33,7 +31,6 @@ MIDDLEWARE = [
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
-    "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
--- config/django/local.py
+++ config/django/local.py
@@ -3,6 +3,15 @@ from .base import env
 
 DEBUG = True
 
+INSTALLED_APPS += [
+    "django.contrib.admin",
+    "django.contrib.messages",
+]
+
+MIDDLEWARE += [
+    "django.contrib.messages.middleware.MessageMiddleware",
+]
+
 SECRET_KEY = env("SECRET_KEY", default="SET_SECRET_KEY")
 
 REST_FRAMEWORK["AUTH_COOKIE_SAME_SITE"] = None
