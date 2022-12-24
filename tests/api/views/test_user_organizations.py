a26930d23fa23d4b3475eb22dbbe928ae470dff8
Tapio Jokinen <tapio.jokinen@hotmail.com>
2022-12-24 16:45:41 +0200

    Add tests for user_organizations API

---
+++ tests/api/views/test_user_organizations.py
@@ -0,0 +1,43 @@
+from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APIRequestFactory, force_authenticate
+
+from nyrkes.api.views.user_organizations import UserOrganizations
+from tests.factories import OrganizationFactory, UserFactory
+
+
+class UserOrganizationTests(TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        super().setUpClass()
+        cls.user = UserFactory()
+        cls.org = OrganizationFactory(owner=cls.user)
+
+    def setUp(self) -> None:
+        self.factory = APIRequestFactory()
+        self.view = UserOrganizations.as_view()
+
+    def test_user_organizations(self):
+        path = reverse("user_organizations")
+        request = self.factory.get(
+            path,
+            HTTP_ACCEPT="application/json; version=1.0",
+            HTTP_CONTENT_TYPE="application/json",
+        )
+        force_authenticate(request, self.user)
+        response = self.view(request)
+
+        correct_keys = [
+            "id",
+            "dateAdded",
+            "dateUpdated",
+            "name",
+            "altName",
+            "ownerId",
+        ]
+
+        for k in response.data[0].keys():
+            self.assertIn(k, correct_keys)
+            correct_keys.remove(k)
+
+        self.assertTrue(len(correct_keys) == 0)
